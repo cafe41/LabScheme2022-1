3e66a9f1338ca4f37d90976f8582f0457873e50a
Gustavo Vergara <gustavo.vergara.p@usach.cl>
2022-04-02 22:35:50 -0400

    Segundo commit
    Agregué imports al código y creé los archivos a importar.

--- main_19810849-9_VergaraParra.rkt
+++ main_19810849-9_VergaraParra.rkt
@@ -1,2 +1,19 @@
 #lang racket
 ;Version hasta la fecha 01/04 del laboratorio en Scheme, con Racket
+
+;-------------------TDA--------------------;
+
+
+
+;---------IMPORTACIÓN DE FUNCIONES---------;
+;Importaré archivos de la misma carpeta en donde se encuentra main, ya que asumo que harán "pull" del repositorio con todos los archivos.
+;
+;importamos generarCartas, el cuál contiene las funciones que generan el mazo de cartas
+(require "generarCartas_19810849-9_VergaraParra.rkt")
+;importamos setCartas, archivo con funcionas que comprueban que el set esté bien hecho.
+(require "setCartas_19810849-9_VergaraParra.rkt")
+;importamos modosDeJuego, el cuál contiene las funciones que definen y eligen el modo de juego
+(require "modosDeJuego_19810849-9_VergaraParra.rkt")
+;
+(provide (all-defined-out))
+;---
---
+++ modosDeJuego_19810849-9_VergaraParra.rkt
@@ -0,0 +1,4 @@
+#lang racket
+;Esta parte del código busca resolver el problema de los modos de juego y relacionados.
+;Resuelve el problema 7
+(define c(/ 5 5))
